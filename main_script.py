af4d3f7a9c8072d13a2e47c1c27d1f1b9a7b05dc
ElDonLuna <o.luna2002@gmail.com>
2025-02-27 07:58:35 -0500

    Add files via upload

---
+++ main_script.py
@@ -0,0 +1,42 @@
+#import all files
+
+
+
+
+#establish connection to LAN 
+ 
+ 
+ 
+
+#loop starts:
+
+#read all sensor data
+
+#display data
+
+#check if running on battery or mains
+
+#running on battery
+	#notify user at 10% incraments 
+	
+	#run battery analytics
+	
+	#display data
+	
+	#check if they are above critical point
+	
+	#If below critical point 
+		#if LAN avalible
+			#run automated browser
+			
+		#else hard connection availible 
+			#send fault signal to connected devices 
+			
+		#else 
+			#idk tbh
+			
+		#self shutdown --------this is okay because the pi its self does not control the UPS
+	
+	
+
+
