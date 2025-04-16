606a749470605fce857cb1687b67bea3ad5ec18c
GriffinFrame <griffin@griffinframe.com>
2025-04-16 14:18:14 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,114 @@
+import time
+
+
+from functions.getGMTTime                           import getDateTime
+from functions.sensor_simulate_discharge_non_lin    import getNewDataUNPWR
+from functions.sensor_getRNGData                    import getRNGData
+from functions.nonlinearVoltageToBatPercent         import getBatEstamate
+
+
+
+while 1: # the big main loop
+    
+    try: #initialize the bool for if their is mains
+        if init:
+            #why wont it let me put an empty if
+            #im trying to get it to discover its NULL
+            1+1
+    except:
+        init = True
+        powered = True
+        data = [0.9,0,0,20,0,0,0,0,0,0]
+    
+    
+    
+    try: #create new log file each 10 min and init if the program has just started or errored
+        if time.time() >= ctime+600:
+            ctime = time.time()
+            datalog.close()
+            datalog = open("/var/www/html/logs/" + getDateTime(1) + ".csv",'a')
+    except:
+        datalog = open("/var/www/html/logs/" + getDateTime(1) + ".csv",'a')
+        ctime = time.time()
+    
+    
+    
+    
+    
+    
+    #data[] = bat%,vb,ib,rb,vo,io,vi,ii,vrec,irec
+    #data = getData() # get sim data 
+    data = getNewDataUNPWR(data , 1) # updata sim data
+    
+    
+    #----the getting data script should be replaced with the finalized version of ADS1115_sensor_read.py
+    
+
+
+    #data = getBatEstamate(data)
+    #----get battery percentage using battery_prediction_model.py  when it gets finalized
+    #----put that variable somewhere
+    # i think that we should run the battery prediction model on a separate thread and have it
+    # update the main program when it can so we can not be time constrained by it and log more data
+    
+    print(data)# output for testing purposes this is not needed on the final program
+    
+    
+    
+    #----this is currently working on voltage and should be swaped to bat%
+    if data[1] <= 23: #shuts down the sytem 
+        print("battery undervoltage")
+        print("shuting down")
+        exit()
+        #---- push that the system is shutting down to clients 
+        #----i know im forgetting something that needs to be done here
+        
+    if (data[6] < 112) & powered: #mains failure 
+        print("input undervoltage")
+        print("moving to battery")
+        powered = False
+        #----turn off battery charger
+        #the rest is passively switched
+        
+    if (data[6] > 120) & powered: #over voltage protection
+        print("input overvoltage")
+        print("moving to battery")
+        powered = False
+        #----turn off battery charger
+        #nothing can be done for the power supply currently
+        
+        
+    if (data[6] >= 112) & (data[6] <= 120) & (not powered):
+        print("input restored")
+        print("tranfering to mains")
+        powered = True
+        #----turn charger back on 
+        
+    
+            
+        
+        
+    
+    #log files
+    datalog.write(getDateTime(1)) #log the time
+    datalog.write(", ") #buffer
+    datalog.write(str(data).strip("[]")) #log the data
+    #---- add the bat%
+    datalog.write("\n") #seperate data logs
+    
+    
+    while 1:#wright current data to file
+        try:# since this file will be opened by another program it might error so it will wait a little if it fails
+            currentdata = open("/var/www/html/data.csv",'w')
+            currentdata.write(str(data).strip("[]"))
+            currentdata.close()
+            break
+        except:
+            time.sleep(0.25)
+    
+        
+    time.sleep(1.5) #delay for testing purposes to not swamp the logs
+    # i belive that once all the code is running together the 
+    # time it takes to run the code can replace this
+
+    
